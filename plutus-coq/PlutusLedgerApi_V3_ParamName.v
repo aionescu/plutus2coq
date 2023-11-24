83301621218b7ffeda122c2ce79cd807854aa078
Alex Ionescu <aaionescu@pm.me>
2023-11-24 04:06:31 +0100

    Remove extraction-related directives from Preamble

--- plutus-coq/PlutusLedgerApi_V3_ParamName.v
+++ plutus-coq/PlutusLedgerApi_V3_ParamName.v
@@ -12,16 +12,6 @@ Require Coq.Program.Wf.
 
 (* Preamble *)
 
-From Coq Require Extraction.
-From Coq Require ExtrHaskellBasic.
-From Coq Require ExtrHaskellNatInteger.
-From Coq Require ExtrHaskellZInteger.
-From Coq Require ExtrHaskellString.
-
-Extraction Language Haskell.
-Unset Extraction Optimize.
-Set Extraction KeepSingleton.
-
 Require GHC.Base.
 Import GHC.Base.Notations.
 
